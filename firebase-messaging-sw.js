57a9bbc952c6a1dca77774ed742facb6470f5e75
yuga <>
2023-04-20 10:42:59 +0800

    no message

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,32 +1,50 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging, getToken } from "firebase/messaging/sw";
-
+import { initializeApp } from 'firebase/app';
+import { getMessaging, onMessage } from 'firebase/messaging';
+import { onBackgroundMessage } from "firebase/messaging/sw";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDEYWHma9rEroGOs5XYe8gZaDax8TIWRMo",
-  authDomain: "test-58de1.firebaseapp.com",
-  databaseURL: "https://test-58de1-default-rtdb.firebaseio.com",
-  projectId: "test-58de1",
-  storageBucket: "test-58de1.appspot.com",
-  messagingSenderId: "439805881996",
-  appId: "1:439805881996:web:41a120bee954577e1895a9"
+    apiKey: "AIzaSyDEYWHma9rEroGOs5XYe8gZaDax8TIWRMo",
+    authDomain: "test-58de1.firebaseapp.com",
+    databaseURL: "https://test-58de1-default-rtdb.firebaseio.com",
+    projectId: "test-58de1",
+    storageBucket: "test-58de1.appspot.com",
+    messagingSenderId: "439805881996",
+    appId: "1:439805881996:web:41a120bee954577e1895a9"
 };
 
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
-const VAPID_KEY = 'BCn-idO9957h3uKL4NDyjLZgm8Xh9_wWFAu0oQEgiDwXzZpv4oui3Iw3MTZd7T2iUZzarv_H-rRZZwlAHkVu0bo'
-getToken(messaging, {vapidKey: VAPID_KEY}).then((currentToken) => {
-  console.log('getToken-firebase-Messaging-sw')
-  console.log(currentToken)
-}).catch((err) => { console.log('檢索token時出錯', err);})
 
-
-
-self.addEventListener("push", (event) => {
-  const payload = event.data.json();
-  const { title, body } = payload.notification;
-  const options = {
-    body,
-  };
-  event.waitUntil(self.registration.showNotification(title, options));
+// 前景
+onMessage(messaging, (payload) => {
+  console.log('Message received:', payload);
 });
+onBackgroundMessage(messaging, (payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+      body: 'Background Message body.',
+      icon: 'images/144x144.png'
+    };
+  
+    self.registration.showNotification(notificationTitle,
+      notificationOptions);
+  });
+// const app = initializeApp(firebaseConfig);
+// const messaging = getMessaging(app);
+// getToken(messaging, {vapidKey: 'BCn-idO9957h3uKL4NDyjLZgm8Xh9_wWFAu0oQEgiDwXzZpv4oui3Iw3MTZd7T2iUZzarv_H-rRZZwlAHkVu0bo'}).then((currentToken) => {
+//   console.log('getToken-firebase-Messaging-sw')
+//   console.log(currentToken)
+// }).catch((err) => { console.log('檢索token時出錯', err);})
+
+
+
+// self.addEventListener("push", (event) => {
+//   const payload = event.data.json();
+//   const { title, body } = payload.notification;
+//   const options = {
+//     body,
+//   };
+//   event.waitUntil(self.registration.showNotification(title, options));
+// });
--- sw.js
+++ sw.js
@@ -1,36 +1,4 @@
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js')
-importScripts(
-  'https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js'
-)
-import { onBackgroundMessage } from 'firebase/messaging/sw'
-import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging";
-const firebaseApp = initializeApp({
-  apiKey: "AIzaSyDEYWHma9rEroGOs5XYe8gZaDax8TIWRMo",
-  authDomain: "test-58de1.firebaseapp.com",
-  databaseURL: "https://test-58de1-default-rtdb.firebaseio.com",
-  projectId: "test-58de1",
-  storageBucket: "test-58de1.appspot.com",
-  messagingSenderId: "439805881996",
-  appId: "1:439805881996:web:41a120bee954577e1895a9"
-});
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = getMessaging(firebaseApp);
-
-onBackgroundMessage(messaging, ({ notification }) => {
-  const { title, body, image } = notification ?? {}
-
-  if (!title) {
-    return
-  }
-
-  self.registration.showNotification(title, {
-    body,
-    icon: image
-  })
-})
 
 // Service Worker 安裝事件
 self.addEventListener('install', () => {
@@ -51,13 +19,4 @@ self.addEventListener('push', event => {
 // Service Worker 接收 Firebase Cloud Messaging 消息事件
 self.addEventListener('message', event => {
   console.log('接收到 Firebase Cloud Messaging 消息：', event.data);
-
-  // 註冊回調函數，當接收到新消息時觸發
-  messaging.onMessage(payload => {
-    console.log('接收到新的 Firebase Cloud Messaging 消息：', payload);
-  });
 });
-
-
-
-
